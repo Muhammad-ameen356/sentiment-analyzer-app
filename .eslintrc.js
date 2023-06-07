52e021ea9b834d771b2d789f9f962892e38d95a9
muhammad-ameen356 <mameen@nisum.com>
2023-06-07 18:05:55 +0500

    Audio record library

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:jest/recommended",
-    "airbnb",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "plugin:jest/recommended", "airbnb", "prettier"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
@@ -21,12 +16,11 @@ module.exports = {
     "import/prefer-default-export": 0,
     "default-param-last": 0,
     "react/react-in-jsx-scope": 0,
-    "react/jsx-filename-extension": ["error", {extensions: [".js"]}],
+    "react/jsx-filename-extension": ["error", { extensions: [".js"] }],
     "react/jsx-uses-react": 0,
-    "import/no-extraneous-dependencies": ["error", {devDependencies: true}],
     "import/no-unresolved": 0,
     "react/prop-types": 0,
-    "no-use-before-define": ["error", {variables: false}],
+    "no-use-before-define": ["error", { variables: false }],
     "react/function-component-definition": [
       2,
       {
@@ -35,6 +29,6 @@ module.exports = {
       },
     ],
     "react/jsx-props-no-spreading": 0,
-    "no-param-reassign": [2, {props: false}],
+    "no-param-reassign": [2, { props: false }],
   },
 };
--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -1,13 +1,44 @@
+/* eslint-disable no-unused-vars */
 import { BaseSafeAreaView } from "components";
 import { theme, micIcon, unitedFlag } from "constants";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  Image,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Voice from "@react-native-voice/voice";
-import { useEffect, useState } from "react";
-import { microphonePermission } from "utils";
+import { useEffect, useId, useState } from "react";
+import { microphonePermission, multiplePermissionForRecordAudio } from "utils";
+import AudioRecorderPlayer from "react-native-audio-recorder-player";
+import RNFetchBlob from "rn-fetch-blob";
+
+const audioRecorderPlayer = new AudioRecorderPlayer();
 
 const Dashboard = () => {
   const [isRecordingStart, setIsRecordingStart] = useState(false);
   const [resultState, setResultState] = useState("");
+  const [state, setState] = useState();
+  const uniqueId = useId();
+
+  const { dirs } = RNFetchBlob.fs;
+  const path = Platform.select({
+    ios: "hello.m4a",
+    android: `${dirs.CacheDir}/${uniqueId}_analyzer.mp3`,
+  });
+
+  const onRecordingStart = () => {
+    setIsRecordingStart(true);
+  };
+
+  const onRecordingEnd = () => {
+    setIsRecordingStart(false);
+  };
 
   useEffect(() => {
     Voice.onSpeechStart = onSpeechStartHandler;
@@ -19,54 +50,134 @@ const Dashboard = () => {
     };
   }, []);
 
-  Voice.onSpeechResults = (result) => setResultState(result.value[0]);
-  Voice.onSpeechEnd = () => setIsRecordingStart(false);
-
-  console.log(resultState, "resultState");
-
   const onSpeechStartHandler = (e) => {
     console.log(e, "start");
   };
 
   const onSpeechEndHandler = (e) => {
     console.log(e, "end");
+    onRecordingEnd();
   };
 
   const onSpeechResultsHandler = (e) => {
     console.log(e, "result");
   };
 
-  const startRecording = async () => {
-    const permissionResult = await microphonePermission();
+  const onSpeechVolumeChangedHandler = (e) => {
+    // console.log("onSpeechVolumeChanged: ", e);
+  };
 
+  const startRecognizing = async () => {
+    const permissionResult = await microphonePermission();
     if (permissionResult) {
       try {
         await Voice.start("en-Us");
-        setIsRecordingStart(true);
+        onRecordingStart();
       } catch (error) {
-        setIsRecordingStart(false);
+        onRecordingEnd();
         console.log(error);
       }
     }
   };
 
-  const stopRecording = async () => {
+  const stopRecognizing = async () => {
     try {
-      setIsRecordingStart(false);
       await Voice.stop();
     } catch (error) {
-      setIsRecordingStart(false);
       console.log(error);
+    } finally {
+      onRecordingEnd();
     }
   };
 
-  const onSpeechVolumeChangedHandler = (e) => {
-    console.log("onSpeechVolumeChanged: ", e);
-  };
+  // For Recording
+
+  // const startRecording = async () => {
+  //   onRecordingStart();
+
+  //   const recordPermission = await multiplePermissionForRecordAudio();
+  //   console.log(recordPermission, "recordPermission");
+  //   if (recordPermission) {
+  //     const result = await audioRecorderPlayer.startRecorder(path);
+
+  //     audioRecorderPlayer.addRecordBackListener((e) => {
+  //       setState((prevState) => ({
+  //         ...prevState,
+  //         recordSecs: e.currentPosition,
+  //         recordTime: audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)),
+  //       }));
+  //     });
+  //     console.log(result, "RESULT");
+  //     console.log(state, "state");
+  //   }
+  // };
+
+  // const stopRecording = async () => {
+  //   const result = await audioRecorderPlayer.stopRecorder();
+  //   audioRecorderPlayer.removeRecordBackListener();
+  //   setState((prevState) => ({
+  //     ...prevState,
+  //     recordSecs: 0,
+  //   }));
+  //   console.log(result);
+  //   onRecordingEnd();
+  // };
+
+  // const onStartPlay = async () => {
+  //   const msg = await audioRecorderPlayer.startPlayer(path);
+  //   console.log(msg);
+  //   audioRecorderPlayer.addPlayBackListener((e) => {
+  //     setState((prevState) => ({
+  //       ...prevState,
+  //       currentPositionSec: e.currentPosition,
+  //       currentDurationSec: e.duration,
+  //       playTime: audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)),
+  //       duration: audioRecorderPlayer.mmssss(Math.floor(e.duration)),
+  //     }));
+  //   });
+  // };
+
+  // const onPausePlay = async () => {
+  //   await audioRecorderPlayer.pausePlayer();
+  // };
+
+  // const onStopPlay = () => {
+  //   audioRecorderPlayer.stopPlayer();
+  //   audioRecorderPlayer.removePlayBackListener();
+  //   setState((prevState) => ({
+  //     ...prevState,
+  //     currentPositionSec: 0,
+  //     currentDurationSec: 0,
+  //   }));
+  // };
+
+  const DATA = [
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      title: "First Item",
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: "Second Item",
+    },
+    {
+      id: "58694a11110f-3da1-471f-bd96-145571e29d72",
+      title: "Third Item",
+    },
+    {
+      id: "586942222a0f-3da1-471f-bd96-145571e29d72",
+      title: "Fourth Item",
+    },
+    {
+      id: "58694a3330f-3da1-471f-bd96-145571e29d72",
+      title: "Fifth Item",
+    },
+  ];
 
   return (
     <View style={styles.main}>
-      <BaseSafeAreaView>
+      {/* <BaseSafeAreaView> */}
+      <SafeAreaView>
         <View>
           <View style={styles.firstContainer}>
             <Text style={styles.firstContainerText}>
@@ -88,13 +199,51 @@ const Dashboard = () => {
             <View style={styles.historyBTN}>
               <Text style={styles.history_text}>History</Text>
               <TouchableOpacity>
-                <Text style={styles.history_text}>View all</Text>
+                <Text style={[styles.history_text, { textDecorationLine: "underline" }]}>
+                  View all
+                </Text>
               </TouchableOpacity>
             </View>
-            <View style={styles.resultBoxMain} />
+            <View style={styles.resultBoxMain}>
+              <FlatList
+                data={DATA}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item, index }) => (
+                  <View
+                    style={[
+                      styles.historyBox1,
+                      index === 0
+                        ? { marginVertical: 10, marginLeft: 0, marginRight: 10 }
+                        : { margin: 10 },
+                    ]}
+                  >
+                    <View style={styles.resultBoxTextView}>
+                      <Text style={styles.resultBoxText}>{item?.title}</Text>
+                    </View>
+                    <View style={styles.resultBoxScore}>
+                      <Text style={styles.resultBoxScoreText}>2 / 5</Text>
+                    </View>
+                  </View>
+                )}
+                keyExtractor={(item) => item.id}
+              />
+            </View>
           </View>
           <View style={styles.fourthContainer}>
-            <TouchableOpacity onPress={isRecordingStart ? stopRecording : startRecording}>
+            <TouchableOpacity
+              onPress={
+                isRecordingStart
+                  ? () => {
+                      stopRecognizing();
+                      // stopRecording();
+                    }
+                  : () => {
+                      startRecognizing();
+                      // startRecording();
+                    }
+              }
+            >
               <View style={styles.micIconBox}>
                 {isRecordingStart ? (
                   <View style={styles.stopMicIcon} />
@@ -103,9 +252,12 @@ const Dashboard = () => {
                 )}
               </View>
             </TouchableOpacity>
+            {/* <Button title="PLAY" onPress={onStartPlay} />
+            <Button title="STOP" onPress={onStopPlay} /> */}
           </View>
         </View>
-      </BaseSafeAreaView>
+      </SafeAreaView>
+      {/* </BaseSafeAreaView> */}
     </View>
   );
 };
@@ -132,6 +284,7 @@ const styles = StyleSheet.create({
     height: "20%",
     justifyContent: "center",
     alignItems: "center",
+    flexDirection: "row",
   },
   firstContainerText: {
     fontSize: 18,
@@ -152,15 +305,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: theme.primary_text_color,
   },
-  resultBoxMain: {
-    height: "80%",
-    marginLeft: 30,
-    marginRight: 30,
-    backgroundColor: theme.primary_color,
-    borderRadius: 15,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+
   micIconBox: {
     justifyContent: "center",
     alignItems: "center",
@@ -212,5 +357,38 @@ const styles = StyleSheet.create({
   history_text: {
     color: theme.white_color,
     fontSize: 16,
+  },
+  resultBoxMain: {
+    height: "80%",
+    marginLeft: 30,
+    marginRight: 30,
+    borderRadius: 15,
+  },
+  historyBox1: {
+    width: 200,
+    height: "90%",
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: theme.primary_color,
+  },
+  resultBoxTextView: {
+    height: "80%",
+  },
+  resultBoxText: {
+    color: theme.white_color,
+  },
+  resultBoxScore: {
+    height: "20%",
+    alignItems: "flex-end",
+    borderTopColor: theme.primary_text_color,
+    borderTopWidth: 0.5,
+  },
+  resultBoxScoreText: {
+    color: theme.secondary_color,
+    fontSize: 14,
+    paddingRight: 10,
+    marginTop: 7,
+    fontWeight: "bold",
   },
 });
